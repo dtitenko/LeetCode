fb4d9451f51cb4a9d81f6266a30071873a536e21
Dmitry Titenko <d.titenko@evision-software.com>
2019-09-18 10:19:44 +0200

    Add logging to the Subarrays solution

--- csharp/SubarraysWIthDifferentIntegers/Program.cs
+++ csharp/SubarraysWIthDifferentIntegers/Program.cs
@@ -1,71 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace SubarraysWIthDifferentIntegers
+// ReSharper disable once CheckNamespace
+internal static class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            var solution = new Solution();
-            // A = [1,2,1,2,3], K = 2
-            // var A = new[] { 2, 1, 2, 1, 2 };
-            var A = new[] {1, 2, 1, 2, 3};
-            var K = 2;
-            var res = solution.SubarraysWithKDistinct(A, K);
-            Console.WriteLine(res);
-        }
+        var solution = new Solution();
+        // A = [1,2,1,2,3], K = 2
+        // var A = new[] { 2, 1, 2, 1, 2 };
+        var A = new[] { 1, 2, 1, 2, 3 };
+        var K = 2;
+        var res = solution.SubarraysWithKDistinct(A, K);
+        Console.WriteLine(res);
     }
+}
 
-    public class Solution
+public class Solution
+{
+    public int SubarraysWithKDistinct(int[] A, int K)
     {
-        public int SubarraysWithKDistinct(int[] A, int K)
+        var n = A.Length;
+        var res = 0;
+        var unique = new Dictionary<int, int>();
+        var rightWindow = new Dictionary<int, int>();
+        int left1 = 0;
+        int left2 = 0;
+
+        for (int right = 0; right < n; right++)
         {
-            var n = A.Length;
-            var res = 0;
-            var map1 = new Dictionary<int, int>();
-            var map2 = new Dictionary<int, int>();
-            int left1 = 0;
-            int left2 = 0;
+            int val = A[right];
+            Console.WriteLine($"=== {val} ===");
+            unique[val] = unique.GetValueOrDefault(val, 0) + 1;
+            rightWindow[val] = rightWindow.GetValueOrDefault(val, 0) + 1;
 
-            for (int right = 0; right < n; right++)
+            while (unique.Count > K)
             {
-                int val = A[right];
-                map1[val] = map1.GetValueOrDefault(val, 0) + 1;
-                map2[val] = map2.GetValueOrDefault(val, 0) + 1;
-
-                while (map1.Count > K)
+                var leftVal = A[left1];
+                if (unique.TryGetValue(leftVal, out var leftCount))
                 {
-                    int l1 = map1.GetValueOrDefault(A[left1], 0);
-                    if (l1 - 1 <= 0)
-                    {
-                        map1.Remove(A[left1]);
-                    }
-                    else
-                    {
-                        map1[A[left1]] = l1 - 1;
-                    }
-                    left1++;
+                    if (leftCount > 1) unique[leftVal] = leftCount - 1;
+                    else unique.Remove(leftVal);
                 }
 
-                while (map2.Count >= K)
-                {
-                    int l2 = map2.GetValueOrDefault(A[left2], 0);
-                    if (l2 - 1 <= 0)
-                    {
-                        map2.Remove(A[left2]);
-                    }
-                    else
-                    {
-                        map2[A[left2]] = l2 - 1;
-                    }
-                    left2++;
-                }
-
-                res += left2 - left1;
+                left1++;
             }
 
-            return res;
+            while (rightWindow.Count >= K)
+            {
+                var leftVal = A[left2];
+                if (rightWindow.TryGetValue(leftVal, out var leftCount))
+                {
+                    if (leftCount > 1) rightWindow[leftVal] = leftCount - 1;
+                    else rightWindow.Remove(leftVal);
+                }
+
+                left2++;
+            }
+
+            Console.WriteLine("unique: " + string.Join(", ", unique.Select(kv => $"{kv.Key}:{kv.Value}")));
+            Console.WriteLine("rightWindow: " + string.Join(", ", rightWindow.Select(kv => $"{kv.Key}:{kv.Value}")));
+            Console.WriteLine($"{left2} - {left1} = {left2 - left1}");
+
+            res += left2 - left1;
+            Console.WriteLine($"res: {res}");
+
+            Console.WriteLine();
         }
+
+        return res;
     }
 }
