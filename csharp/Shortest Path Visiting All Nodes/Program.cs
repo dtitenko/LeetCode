6369e92941f3753883610370f638de192b7b6180
Dmitry Titenko <d.titenko@evision-software.com>
2019-09-26 22:10:16 +0200

    Splitting

--- csharp/Shortest Path Visiting All Nodes/Program.cs
+++ csharp/Shortest Path Visiting All Nodes/Program.cs
@@ -5,8 +5,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        var input = "[[1],[0,2,4],[1,3,4],[2],[1,2]]".Split(",");
+        var input = "[[1],[0,2,4],[1,3,4],[2],[1,2]]"
+            .Trim('[', ']').Split(',')
+            .Select(s => s.Trim('[', ']').Split(',').Select(int.Parse).ToArray()).ToArray();
         var graph = new Graph(input.Length);
+        Console.WriteLine(graph);
+        return;
         for (var i = 0; i < input.Length; i++)
         {
             foreach (var vertex in input[i])
