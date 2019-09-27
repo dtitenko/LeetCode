cfc7922488846e2e7401eb3e2c97f71ab35a4096
Dmitry Titenko <d.titenko@evision-software.com>
2019-09-27 05:56:35 +0200

    Redoing with child paths comparison

--- csharp/Shortest Path Visiting All Nodes/DepthFirstPathsAllNodes.cs
+++ csharp/Shortest Path Visiting All Nodes/DepthFirstPathsAllNodes.cs
@@ -2,37 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+// 0 - 1 - 2 - 3
+//      \ /
+//       4
 public class DepthFirstPathsAllNodes
 {
     private readonly int _s;
+    private readonly bool[] _marked;
 
-    public DepthFirstPathsAllNodes(Graph G, int s)
+    public DepthFirstPathsAllNodes(Graph graph, int s)
     {
         _s = s;
-        Dfs(G, s, -1);
+        _marked = new bool[graph.Vertices()];
     }
 
-    public List<int> Path { get; } = new List<int>();
-
-    private void Dfs(Graph graph, int v, int from)
+    public List<int> Dfs(Graph graph, int v, int from)
     {
-        Path.Add(v);
+        var path = new List<int>(new[] { v });
+
         var adjacent = graph.AdjacentVertices(v);
         for (int i = 0; i < adjacent.Count; i++)
         {
             var w = adjacent[i];
-            if (w != from)
+            var childPath = Dfs(graph, w, v);
+            if (i + 1 < adjacent.Count)
             {
-                Console.WriteLine($"{w} from {v}");
-                Dfs(graph, w, v);
-                if (i + 1 < adjacent.Count)
-                    Path.Add(v);
+                path.Add(v);
             }
         }
-    }
 
-    public override string ToString()
-    {
-        return string.Join(" -> ", Path);
+        return path;
     }
 }
--- csharp/Shortest Path Visiting All Nodes/Program.cs
+++ csharp/Shortest Path Visiting All Nodes/Program.cs
@@ -1,16 +1,15 @@
 ﻿using System;
 using System.Linq;
+using Microsoft.VisualBasic;
+using Newtonsoft.Json;
 
 class Program
 {
     static void Main(string[] args)
     {
-        var input = "[[1],[0,2,4],[1,3,4],[2],[1,2]]"
-            .Trim('[', ']').Split(',')
-            .Select(s => s.Trim('[', ']').Split(',').Select(int.Parse).ToArray()).ToArray();
+        var input = JsonConvert.DeserializeObject<int[][]>("[[1,2,3],[0],[0],[0]]");
+        // var input = JsonConvert.DeserializeObject<int[][]>("[[1],[0,2,4],[1,3,4],[2],[1,2]]");
         var graph = new Graph(input.Length);
-        Console.WriteLine(graph);
-        return;
         for (var i = 0; i < input.Length; i++)
         {
             foreach (var vertex in input[i])
@@ -21,6 +20,7 @@ class Program
         }
 
         var dfp = new DepthFirstPathsAllNodes(graph, 1);
-        Console.WriteLine(dfp);
+        var path = dfp.Dfs(graph, 0, -1);
+        Console.WriteLine(string.Join(" -> ", path));
     }
 }
