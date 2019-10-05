b6a62bac012ea5a91bb073ba76d9d6a44f31b38c
Dmitry Titenko <d.titenko@evision-software.com>
2019-10-05 14:18:02 +0200

    Find the Duplicate Number

---
+++ csharp/FindDuplicateNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var solution = new Solution();
+        Console.WriteLine(solution.FindDuplicate(JsonConvert.DeserializeObject<int[]>("[1,3,4,2,2]")));
+        Console.WriteLine(solution.FindDuplicate(JsonConvert.DeserializeObject<int[]>("[3,1,3,4,2]")));
+        Console.WriteLine(solution.FindDuplicate(JsonConvert.DeserializeObject<int[]>("[1,1,2]")));
+    }
+}
+
+public class Solution
+{
+    public int FindDuplicate(int[] nums)
+    {
+        var found = new bool[nums.Length - 1];
+        foreach (var num in nums)
+        {
+            if (found[num - 1])
+            {
+                return num;
+            }
+
+            found[num - 1] = true;
+        }
+
+        return -1;
+    }
+}
