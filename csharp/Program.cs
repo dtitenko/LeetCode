8edf96f08452aba77540be6bec852f0e9da007ff
Dmitry Titenko <d.titenko@evision-software.com>
2019-10-15 16:52:25 +0200

    Run single solution, if arguments provided

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -11,62 +11,74 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            var solutions = typeof(Program).GetTypeInfo().Assembly.GetTypes()
+            (string num, ISolution solution)[] solutions = typeof(Program).GetTypeInfo().Assembly.GetTypes()
                 .Where(t => typeof(ISolution).IsAssignableFrom(t) && !t.IsInterface && t.Name != "__Template")
                 .Select(t => (ISolution)Activator.CreateInstance(t))
-                .OrderBy(p => p.Name)
+                .Select(solution => (new string(solution.Name.TakeWhile(c => c != '.').ToArray()), solution))
+                .OrderBy(p => p.solution.Name)
                 .ToArray();
 
-            var program = new Program();
-            program.DisplayMenu(solutions);
+            if (args.Contains("--run"))
+            {
+                var solution = solutions.FirstOrDefault(s => s.num == args[1]);
+                RunSolution(solution);
+            }
+            else
+            {
+                var program = new Program();
+                program.DisplayMenu(solutions);
+            }
         }
 
-        private void DisplayMenu(ISolution[] solutions)
+        private void DisplayMenu((string num, ISolution solution)[] solutions)
         {
             var menu = new Menu("LeetCode");
 
             foreach (var solution in solutions)
             {
-                menu.Add(new Menu.Item(new string(solution.Name.TakeWhile(c => c != '.').ToArray()), solution.Name,
+                menu.Add(new Menu.Item(solution.num, solution.solution.Name,
                     new Menu.Item("1", "Launch LeetCode page",
                         c =>
                         {
-                            Process.Start("explorer.exe", solution.Link);
-                            c.WriteLine($"Opened browser to '{solution.Link}'");
+                            Process.Start("explorer.exe", solution.solution.Link);
+                            c.WriteLine($"Opened browser to '{solution.solution.Link}'");
                         }),
-                    new Menu.Item("2", "Run test cases",
-                        c =>
-                        {
-                            Array cases = ((dynamic)solution).TestCases;
-                            foreach (dynamic @case in cases)
-                            {
-                                var input = @case.Item1;
-                                var expected = @case.Item2;
-                                var expectedStr = JsonConvert.SerializeObject(expected);
-
-                                var result = ((dynamic)solution).Execute(input);
-
-                                var resultStr = JsonConvert.SerializeObject(result);
-                                var originalColor = Console.ForegroundColor;
-                                Console.ForegroundColor =
-                                    expectedStr == resultStr ? ConsoleColor.Green : ConsoleColor.Red;
-                                var report = $"{JsonConvert.SerializeObject(input)} expected: {expectedStr}, result {resultStr}";
-                                if (report.Length > Console.LargestWindowWidth)
-                                {
-                                    report = $@"{JsonConvert.SerializeObject(input)}
-- expected: {expectedStr}
-- result {resultStr}";
-                                }
-                                Console.WriteLine(report);
-                                Console.ForegroundColor = originalColor;
-                            }
-                        }),
+                    new Menu.Item("2", "Run test cases", _ => RunSolution(solution)),
                     Menu.Item.GoUp));
             }
 
             menu.Add(new Menu.Item("q", "Quit", c => c.CloseAndReturn("0")));
 
             menu.Show();
+        }
+
+        private static void RunSolution((string num, ISolution solution) item)
+        {
+            dynamic solution = item.solution;
+            Array cases = solution.TestCases;
+            foreach (dynamic @case in cases)
+            {
+                var input = @case.Item1;
+                var expected = @case.Item2;
+                var expectedStr = JsonConvert.SerializeObject(expected);
+
+                var result = solution.Execute(input);
+
+                var resultStr = JsonConvert.SerializeObject(result);
+                var originalColor = Console.ForegroundColor;
+                Console.ForegroundColor =
+                    expectedStr == resultStr ? ConsoleColor.Green : ConsoleColor.Red;
+                var report = $"{JsonConvert.SerializeObject(input)} expected: {expectedStr}, result {resultStr}";
+                if (report.Length > Console.LargestWindowWidth)
+                {
+                    report = $@"{JsonConvert.SerializeObject(input)}
+- expected: {expectedStr}
+- result {resultStr}";
+                }
+
+                Console.WriteLine(report);
+                Console.ForegroundColor = originalColor;
+            }
         }
     }
 }
