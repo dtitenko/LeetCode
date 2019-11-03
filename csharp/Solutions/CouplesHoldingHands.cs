7b4ab3679b2149002afbff478fc49a575e58984d
Dmitry Titenko <d.titenko@evision-software.com>
2019-11-03 20:50:52 +0100

    765. Couples Holding Hands -> O(n), O(1) memory

--- csharp/Solutions/CouplesHoldingHands.cs
+++ csharp/Solutions/CouplesHoldingHands.cs
@@ -14,6 +14,7 @@ public class CouplesHoldingHands : ISolution<string, int>
         ("[3, 1, 2, 6, 4, 5, 0, 7]", 2),
         ("[6, 1, 2, 3, 8, 5, 4, 7, 0, 9]", 3),
         ("[6,2,1,7,4,5,3,8,0,9]", 3),
+        ("[1,4,0,5,8,7,6,3,2,9]", 3)
     };
 
     public int Execute(string input)
@@ -24,25 +25,27 @@ public class CouplesHoldingHands : ISolution<string, int>
     public int MinSwapsCouples(int[] row)
     {
         int swap = 0;
+        var places = new int[row.Length];
+        for (var i = 0; i < row.Length; i++)
+        {
+            places[row[i]] = i;
+        }
+
         for (var i = 0; i < row.Length; i += 2)
         {
             var el1 = row[i];
             var el2 = el1 % 2 == 0 ? el1 + 1 : el1 - 1;
-            Console.WriteLine($"{row[i]}:{row[i + 1]}");
             if (row[i + 1] == el2) continue;
-            Console.WriteLine($"el2: {el2}");
-            for (int j = i + 1; j < row.Length; j++)
-            {
-                if (row[j] == el2)
-                {
-                    var temp = row[i + 1];
-                    row[i + 1] = row[j];
-                    row[j] = temp;
-                    swap += 1;
-                    Console.WriteLine(JsonConvert.SerializeObject(row));
-                    break;
-                }
-            }
+
+            row[places[el2]] = row[i + 1];
+            row[i + 1] = el2;
+
+            var temp = row[places[el2]];
+            var tempIndex = places[el2];
+            places[el2] = i + 1;
+            places[temp] = tempIndex;
+
+            swap += 1;
         }
 
         return swap;
