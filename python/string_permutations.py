b35a511cf9e905417f2c312c585eb33935678170
Dmitry Titenko <d.titenko@evision-software.com>
2020-03-09 09:57:14 +0100

    Solve substring permutation with Rabin-Karp substring search

--- python/string_permutations.py
+++ python/string_permutations.py
@@ -1,41 +1,37 @@
 # Given a smaller string s and a bigger string b, design an algorithm to find all permutations
 # of the shorter string withing the longer one. Print the locations of each permutation.
 
-def permutations(s = "", b = ""):
-    chars = {char : s.count(char) for char in s}
-    window = dict()
-    right, left, formed, sub = 0, 0, 0, 0
-    while right < len(b):
-        c = b[right]
-        if c not in chars:
-            window.clear()
-            right += 1
-            left = right
-            formed = 0
-            sub = 0
-            continue
-        else:
-            matched = window.get(c, 0) == chars[c]
-            window[c] = window.get(c, 0) + 1
-            sub += 1
-            if window[c] == chars[c]:
-                formed += 1
-            elif matched:
-                formed -= 1
-            if formed == len(chars):
-                print(b[left:right + 1])
-            if sub > len(s):
-                c = b[left]
-                matched = window[c] == chars[c]
-                window[c] -= 1
-                sub -= 1
-                if window[c] == chars[c]:
-                    formed += 1
-                elif matched:
-                    formed -= 1
-                left += 1
-                if formed == len(chars):
-                    print(b[left:right + 1])
-        right += 1
+# using Rabin-Karp Substring Search
+
+def permutations(s="", b=""):
+    sLen = len(s)
+    bLen = len(b)
+    if sLen > bLen:
+        raise Exception('s > b')
+    sHash = 0
+    bHash = 0
+    # prepare first len(s) symbols hash
+    for i in range(sLen):
+        sHash += charHash(s[i])
+        bHash += charHash(b[i])
+    if sHash == bHash and isPermutation(s, b[0:sLen]):
+        print(b[0:sLen])
+    for i in range(sLen, bLen):
+        # slide the window removing hash of the first symbol and adding next symbol
+        bHash = bHash - charHash(b[i - sLen]) + charHash(b[i])
+        start = i - sLen + 1
+        if sHash == bHash and isPermutation(s, b[start:start + sLen]):
+            print(b[start:start + sLen])
+
+
+def charHash(char):
+    return ord(char) % 26
+
+def isPermutation(s1, s2):
+    if len(s1) != len(s2) or len(s1) == 0:
+        return False
+    if sorted(s1) == sorted(s2):
+        return True
+
 
 permutations("abbc", "cbabadcbbabbcbabaabccbabc")
